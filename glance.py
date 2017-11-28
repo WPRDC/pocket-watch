4e57b1e2fd84ad489a507517f554e25f7763e233
drw <drw@users.noreply.github.com>
2017-11-28 16:48:24 -0500

    Save list of stale packages from the last scan to a JSON file

--- glance.py
+++ glance.py
@@ -8,7 +8,7 @@
 # recently modified resource with the nominal publication frequency
 # and the current date.
 
-import requests, textwrap
+import json, requests, textwrap
 
 from datetime import datetime, timedelta
 from pprint import pprint
@@ -107,5 +107,19 @@ print("\n\nStale Datasets by Refresh-by Date: ")
 print_table(stale_ps_by_recency)
 
 
+# Store list of stale packages in a JSON file as a record of the last 
+# glance (with the intent of sending notifications whenever new ones show up).
+output = []
+for sp in stale_ps_by_recency:
+    r = {'id': sp[0], 'title': sp[1]['title']}
+    output.append(r)
+
+with open('last_scan.json', 'w') as f:
+    json.dump(output, f, ensure_ascii=True, indent = 4)
+
+###############
+
 coda = "Out of {} packages, only {} have specified publication frequencies. {} are stale (past their refresh-by date), according to the metadata_modified field.".format(len(packages),packages_with_frequencies,stale_count)
 print(textwrap.fill(coda,70))
+
+
