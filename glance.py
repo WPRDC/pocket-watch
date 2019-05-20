18c68a7fd3da9739d0864508d816c6c52e4f92f5
drw <drw@users.noreply.github.com>
2019-05-20 16:02:08 -0400

    Add holiday-computation functions copied from park-shark

--- glance.py
+++ glance.py
@@ -187,6 +187,50 @@ def get_scheduled_gaps(package):
             return json.loads(extras['no_updates_on'])
     return []
 
+##  BEGIN date/holiday functions obtained from park_shark  ##
+def nth_m_day(year,month,n,m):
+    # m is the day of the week (where 0 is Monday and 6 is Sunday)
+    # This function calculates the date for the nth m-day of a
+    # given month/year.
+    first = date(year,month,1)
+    day_of_the_week = first.weekday()
+    delta = (m - day_of_the_week) % 7
+    return date(year, month, 1 + (n-1)*7 + delta)
+
+def last_m_day(year,month,m):
+    last = date(year,month,monthrange(year,month)[1])
+    while last.weekday() != m:
+        last -= timedelta(days = 1)
+    return last
+
+def is_holiday(date_i):
+    year = date_i.year
+    holidays = [date(year,1,1), #NEW YEAR'S DAY
+    ### HOWEVER, sometimes New Year's Day falls on a weekend and is then observed on Monday. If it falls on a Saturday (a normal non-free parking day), what happens?
+
+    # Really an observed_on() function is needed to offset
+    # some holidays correctly.
+
+        nth_m_day(year,1,3,0),#MARTIN LUTHER KING JR'S BIRTHDAY (third Monday of January)
+        easter(year)-timedelta(days=2),#GOOD FRIDAY
+        last_m_day(year,5,0),#MEMORIAL DAY (last Monday in May)
+        date(year,7,4),#INDEPENDENCE DAY (4TH OF JULY)
+        # [ ] This could be observed on a different day.
+        nth_m_day(year,9,1,0),#LABOR DAY
+        # [ ] This could be observed on a different day.
+        date(year,11,11),#VETERANS' DAY
+        # [ ] This could be observed on a different day.
+
+        nth_m_day(year,11,4,3),#THANKSGIVING DAY
+        #nth_m_day(year,11,4,4),#DAY AFTER THANKSGIVING # Most city departments do not observe this one.
+        date(year,12,25),#CHRISTMAS DAY
+        # [ ] This could be observed on a different day.
+        #date(year,12,26) # Most city departments do not observe this one.
+        ]#DAY AFTER CHRISTMAS
+        # [ ] This could be observed on a different day.
+    return date_i in holidays
+##  END date/holiday functions obtained from park_shark  ##
+
 def compute_lateness(extensions, package_id, publishing_period, reference_dt, no_updates_on=[]):
     effective_reference_dt = account_for_gaps(reference_dt, no_updates_on)
     lateness = datetime.now() - (effective_reference_dt + publishing_period)
