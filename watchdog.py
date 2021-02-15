9aa82350730f9b802f4ad6466f448d3574b41281
drw <drw@users.noreply.github.com>
2021-02-15 14:28:31 -0500

    Prevent checking of private datasets

--- watchdog.py
+++ watchdog.py
@@ -277,18 +277,19 @@ def main(just_testing):
     # should probably be the one that is most representative of the datetime of the event
     # represented by that row.
     for package in packages:
-        if 'extras' in package:
-            extras_list = package['extras']
-            # Keep definitions and uses of extras metadata updated here:
-            # https://github.com/WPRDC/data-guide/blob/master/docs/metadata_extras.md
-            # The format is like this:
-            #       u'extras': [{u'key': u'dcat_issued', u'value': u'2014-01-07T15:27:45.000Z'}, ...
-            # not a dict, but a list of dicts.
-            extras = {d['key']: d['value'] for d in extras_list}
-            #if 'dcat_issued' not in extras:
-            if 'time_field' in extras:
-                time_field_lookup = json.loads(extras['time_field'])
-                fix_temporal_coverage(package['id'],time_field_lookup,just_testing)
+        if not package['private']: # Ignore private packages
+            if 'extras' in package:
+                extras_list = package['extras']
+                # Keep definitions and uses of extras metadata updated here:
+                # https://github.com/WPRDC/data-guide/blob/master/docs/metadata_extras.md
+                # The format is like this:
+                #       u'extras': [{u'key': u'dcat_issued', u'value': u'2014-01-07T15:27:45.000Z'}, ...
+                # not a dict, but a list of dicts.
+                extras = {d['key']: d['value'] for d in extras_list}
+                #if 'dcat_issued' not in extras:
+                if 'time_field' in extras:
+                    time_field_lookup = json.loads(extras['time_field'])
+                    fix_temporal_coverage(package['id'],time_field_lookup,just_testing)
 
 from credentials import production
 try:
